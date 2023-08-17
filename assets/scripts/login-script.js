bc8beca7ff63389fba543770cbc1ee55a4e55817
artha064 <99887958+artha064@users.noreply.github.com>
2023-08-17 10:21:26 +0800

    Update login-script.js
    Applying JSDoc documentation style.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,62 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/**
+* Creating a temporary variable to store the login form.
+* @constant {HTMLelement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/**
+* Creating a variable to store the input email element.
+* @constant {HTMLelement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+* Creating a variable to store the input password element.
+* @constant {HTMLelement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Creating a variable to store user's email temporarily.
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+* Creating a variable to store user's password temporarily.
+* @constant {string}
+*/
 const expectedPassword = 'superpassword';
 
+/* Comment : Adding an on-submit event listener to form's button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  * Creating a variable to store email thrown
+  * @constant {string}
+  */
   const email = inputEmailElement.value;
+
+  /**
+  * Creating a variable to store email thrown
+  * @constant {string}
+  */
   const password = inputPasswordElement.value;
+
+  /* Comment : Check if the user submitting the correct email and password */
   if (email == expectedEmail && password == expectedPassword) {
+    
+    /* Comment : If the submitted email and password are correct then redirect the user to the homepage */
     goToHome();
+    
   } else {
+
+    /* Comment : If the submitted email and password are wrong then display a pop-up to the user*/
     showPopUp();
   }
 });
